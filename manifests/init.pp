bb70873e16f2ed8475ae58a678d75670c2973223
Fernando Alvarez <fernando.alvareznoya@bryxx.eu>
2020-04-29 11:18:41 +0200

    Initial

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+# -----------------------------------------------------------------------------------
+# Description: New Relic Infrastructure Agent
+#
+# Reviewer: elserhumano@gmail.com
+#
+# Date: 04/2020
+#
+# -----------------------------------------------------------------------------------
+#   include newrelic_agent
+
+class newrelic_agent {
+
+}
